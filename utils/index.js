f2c46711fc4155f2276f50ceddbcea8e9da39ef2
akshaymalik1995 <akshaymalik191@gmail.com>
2024-07-05 18:20:30 +0530

    chore: Remove unused code and update captcha API endpoint

--- utils/index.js
+++ utils/index.js
@@ -4,23 +4,6 @@ import https from "https";
 import crypto from "crypto";
 
 
-const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-    secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT
-});
-
-
-export async function makeRequest(data) {
-    const url = "https://gateway.eci.gov.in/api/v1/elastic/search-by-epic-from-national-display/";
-    const headers = {
-        "referer": "https://electoralsearch.eci.gov.in/",
-        "origin": "https://electoralsearch.eci.gov.in",
-    }
-    const response = await axios.post(url, data, { httpsAgent, headers });
-    const html = response.data;
-    console.log(html);
-}
-
 
 export async function getCaptcha() {
     const url = "https://gateway-voters.eci.gov.in/api/v1/captcha-service/generateCaptcha"
